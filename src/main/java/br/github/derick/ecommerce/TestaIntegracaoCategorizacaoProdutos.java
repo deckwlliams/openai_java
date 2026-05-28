9e35848d2e8679efd8cc290625a8c5632b602bcb
Derick Williams <deckprogramador@gmail.com>
2026-05-28 19:14:56 -0300

    melhorias na ia

--- src/main/java/br/github/derick/ecommerce/TestaIntegracaoCategorizacaoProdutos.java
+++ src/main/java/br/github/derick/ecommerce/TestaIntegracaoCategorizacaoProdutos.java
@@ -7,26 +7,38 @@ import com.theokanning.openai.service.OpenAiService;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TestaIntegracaoCategorizacaoProdutos {
-     public static void main(String[] args){
-              var user = "folha de papel";
-              var system = """ 
-                      voce e um categoriazador de produtos e deve trazer somente o nome da gategoroia do produto informado
-                       
-                       escolha uma categoria dentra a lista abaixo:
-                       1. higiene pessoal
-                       2. eletronica
-                       3. carros
-                       4. pereciveis
-                       5. outros
-                      
-             ### exemplo de uso:
-             resposta:esporte
-                       
-                       """;
-                      var chave = System.getenv("openai");
-            var service = new OpenAiService(chave, Duration.ofSeconds(30));
+     public static void main(String[] args) {
+         var tcd = new Scanner(System.in);
+
+
+             System.out.println("digite as categorias validas:");
+             var categoria = tcd.nextLine();
+         while (true) {
+             System.out.println("\n digite o nome do produto: ");
+             var user = tcd.nextLine();
+
+
+             var system = """ 
+                              voce e um categoriazador de produtos e deve trazer somente o nome da gategoroia do produto informado
+                     
+                               escolha uma categoria dentra a lista abaixo:
+                           %s
+                     ### exemplo de uso:
+                     resposta:esporte
+                     
+                     ### regras a serem seguidas:
+                     caso o usuario pergunte algo que não seja de categorização de produtos, voce deve responder que nao deve ajuda rpois seu papel e apenas responder a categoria dos produtos
+                     """.formatted(categoria);
+             dispararRequisicao(user, system);
+
+         }
+     }
+     public static void dispararRequisicao(String user, String system){
+         var chave = System.getenv("openai");
+         var service = new OpenAiService(chave, Duration.ofSeconds(30));
          var completionRequest  = ChatCompletionRequest
                  .builder()
                  .model("gpt-5.4-mini")
@@ -34,14 +46,10 @@ public class TestaIntegracaoCategorizacaoProdutos {
                          new ChatMessage(ChatMessageRole.USER.value(), user),
                          new ChatMessage(ChatMessageRole.SYSTEM.value(),system)
                  ))
-                 .n(5)
                  .build();
          service
                  .createChatCompletion(completionRequest)
                  .getChoices()
-                 .forEach(c -> {
-                     System.out.println(c.getMessage().getContent());
-                System.out.println("\n-----------------------------\n");
-                 });
+                 .forEach(c -> System.out.println(c.getMessage().getContent()));
      }
 }
